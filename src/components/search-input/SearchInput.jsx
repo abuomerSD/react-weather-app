8d986602a4c45b450350ec1177a4baa39e7321f5
abuomersd <tayeb1293@yahoo.com>
2025-08-11 18:45:55 +0300

    feature: added search button

--- src/components/search-input/SearchInput.jsx
+++ src/components/search-input/SearchInput.jsx
@@ -1,13 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./search-input.css";
 import { AutoComplete } from "primereact/autocomplete";
 import axios from "axios";
 import { Context } from "../../context";
+import { Toast } from "primereact/toast";
 
 const SearchInput = () => {
   const [value, setValue] = useState("");
   const [items, setItems] = useState([]);
   const { weatherData, setWeatherData } = useContext(Context);
+  const toast = useRef(null);
 
   const WEATHER_API_KEY = "1e3992d30d09886d401078378bf28026";
 
@@ -23,9 +25,11 @@ const SearchInput = () => {
   //   fecth city names from the cities API
   const search = async (e) => {
     try {
+      const query = e.query;
+
       const response = await axios.request({
         ...options,
-        params: { namePrefix: e.query },
+        params: { namePrefix: query },
       });
       const data = response.data.data;
       console.log("data", data);
@@ -38,19 +42,44 @@ const SearchInput = () => {
 
   // update current weather when input changes'
 
-  const updateWeather = async (cityName) => {
+  const updateWeather = async () => {
     try {
       console.log("update current weather...");
 
-      const currentWeatherResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`
-      );
-      console.log("currentWeatherResponse data", currentWeatherResponse.data);
+      // check if input is empty
+      if (value === "") {
+        toast.current.show({
+          severity: "error",
+          summary: "Error",
+          detail: "Please Choose a City First",
+          life: 3000,
+        });
+        return;
+      }
+
+      const currentWeatherResponse = await axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${WEATHER_API_KEY}&units=metric`
+        )
+        .catch((error) => {
+          console.log("req error", error);
+          // check if city not found
+          if (error.status === 404) {
+            toast.current.show({
+              severity: "error",
+              summary: "Error",
+              detail: "Please Choose a Correct City",
+              life: 3000,
+            });
+            return;
+          }
+        });
+      console.log("currentWeatherResponse", currentWeatherResponse);
 
       const forecastWeatherResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${WEATHER_API_KEY}&units=metric`
       );
-      console.log("forecastWeatherResponse data", forecastWeatherResponse.data);
+      console.log("forecastWeatherResponse", forecastWeatherResponse);
 
       const current = currentWeatherResponse.data;
       const forecast = forecastWeatherResponse.data;
@@ -91,20 +120,21 @@ const SearchInput = () => {
 
   return (
     <div className="container">
+      <Toast ref={toast} position="top-right" className="toast" />
       <div className="search-row">
         <AutoComplete
           className="search-input"
           value={value}
-          suggestions={items}
           completeMethod={search}
+          suggestions={items}
           onChange={(e) => setValue(e.value)}
-          onSelect={(e) => {
-            setValue(e.value);
-            console.log("value", e.value);
-            updateWeather(e.value);
-          }}
           placeholder="Enter The City Name"
         />
+      </div>
+      <div className="btn-search-row">
+        <button className="btn-search" onClick={updateWeather}>
+          Get Weather Data
+        </button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // or any other theme
+import "primereact/resources/primereact.min.css"; // core styles
+// import "primeicons/primeicons.css"; // icons
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
